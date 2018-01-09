50cadb54f565d68fa13807c53d3c7fe5880a8e3c
Matei Popa <mpopa@deloittece.com>
2018-01-09 17:36:59 +0200

    updated app_data_v2

--- services/webcluster/app_data_v2.sh
+++ services/webcluster/app_data_v2.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-sudo apt-get update; sudo apt-get install -y nodejs npm
-pushd /home/ubuntu/app && sudo npm install express && nodejs app.js &
+sudo apt-get update; sudo apt-get install -y nodejs npm unzip
+pushd /home/ubuntu && wget https://s3.eu-central-1.amazonaws.com/digital-repo/mattplication/app.zip
+unzip /home/ubuntu/app.zip && pushd /home/ubuntu/app 
+sudo npm install express && nodejs app.js &
 
 sleep 10;
 
--- services/webcluster/main.tf
+++ services/webcluster/main.tf
@@ -50,6 +50,12 @@ resource "aws_security_group" "instance" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
     lifecycle {
         create_before_destroy = true
     }
@@ -58,6 +64,12 @@ resource "aws_security_group" "instance" {
 resource "aws_security_group" "instance2" {
     name = "${var.cluster_name}-wip-170817"
     ingress {
+        from_port = "${var.server_port2}"
+        to_port = "${var.server_port2}"
+        protocol = "tcp"
+        cidr_blocks = ["78.96.101.50/32", "109.166.0.0/16"]
+    }
+    egress {
         from_port = "${var.server_port2}"
         to_port = "${var.server_port2}"
         protocol = "tcp"
@@ -112,13 +124,13 @@ resource "aws_elb" "wip-elb" {
         instance_port = "${var.server_port}"
         instance_protocol = "http"
     }
-    health_check {
-        healthy_threshold = 2
-        unhealthy_threshold = 2
-        timeout = 3
-        interval = 30
-        target = "HTTP:${var.server_port}/"
-    }
+#    health_check {
+#        healthy_threshold = 2
+#        unhealthy_threshold = 10
+#        timeout = 3
+#        interval = 60
+#        target = "HTTP:${var.server_port}/"
+#    }
 }
 
 resource "aws_security_group" "elb" {
